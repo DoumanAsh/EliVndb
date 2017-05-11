bc007c7c3638d11402494c5414a6ea531446729a
Douman <douman@gmx.se>
2017-05-11 19:14:19 +0300

    Add some metadata for publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,10 +1,16 @@
 defmodule EliVndb.Mixfile do
   use Mix.Project
 
+  @description """
+  Kawaii VNDB API wrapper.
+  """
+
   def project do
     [app: :elivndb,
      version: "0.1.0",
      elixir: "~> 1.4",
+     description: @description,
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -16,23 +22,10 @@ defmodule EliVndb.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger, :ssl]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:poison, "~> 3.1"},
@@ -41,4 +34,10 @@ defmodule EliVndb.Mixfile do
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
+
+  defp package do
+    [ maintainers: ["Douman"],
+      licenses: ["MIT"],
+      links: %{ "Github" => "https://github.com/DoumanAsh/EliVndb" } ]
+end
 end
